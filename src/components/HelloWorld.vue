fe0a4a4da47773a75aa7a3ed5c63f5146a925efc
erik06 <erikslagter@gmail.com>
2022-08-23 15:12:43 -0500

    4 files modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="card">
+      <OrganizationChart :value="data2">
+        <template #default="slotProps">
+          <span>{{ slotProps.node.data.label }}</span>
+        </template>
+      </OrganizationChart>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      data2: {
+        key: '0',
+        data: { label: 'F.C. Barcelona' },
+        children: [
+          {
+            key: '0_0',
+            data: { label: 'F.C. Barcelona' },
+            children: [
+              {
+                key: '0_0_0',
+                data: { label: 'Chelsea F.C.' },
+              },
+              {
+                key: '0_0_1',
+                data: { label: 'F.C. Barcelona' },
+              },
+              {
+                key: '0_0_1',
+                data: { label: 'F.C. Barcelona' },
+                children: [
+                  {
+                    key: '0_0_0',
+                    data: { label: 'Chelsea F.C.' },
+                  },
+                  {
+                    key: '0_0_1',
+                    data: { label: 'F.C. Barcelona' },
+                  },
+                  {
+                    key: '0_0_1',
+                    data: { label: 'F.C. Barcelona' },
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            key: '0_1',
+            data: { label: 'Real Madrid' },
+            children: [
+              {
+                key: '0_1_0',
+                data: { label: 'Bayern Munich' },
+              },
+              {
+                key: '0_1_1',
+                data: { label: 'Real Madrid' },
+              },
+            ],
+          },
+        ],
+      },
+    };
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style scoped lang="scss">
+::v-deep(.p-organizationchart) {
+  .p-person {
+    padding: 0;
+    border: 0 none;
+  }
+
+  .node-header,
+  .node-content {
+    padding: 0.5em 0.7rem;
+  }
+
+  .node-header {
+    background-color: #495ebb;
+    color: #ffffff;
+  }
+
+  .node-content {
+    text-align: center;
+    border: 1px solid #495ebb;
+  }
+
+  .node-content img {
+    border-radius: 50%;
+  }
+
+  .department-cfo {
+    background-color: #7247bc;
+    color: #ffffff;
+  }
+
+  .department-coo {
+    background-color: #a534b6;
+    color: #ffffff;
+  }
+
+  .department-cto {
+    background-color: #e9286f;
+    color: #ffffff;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+import "primeflex/primeflex.css";
+import "primevue/resources/themes/lara-light-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 import {createApp} from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
+import OrganizationChart from 'primevue/organizationchart';
 const app = createApp(App);
 
 app.use(PrimeVue);
-
+app.component('OrganizationChart', OrganizationChart);
 app.mount('#app')
